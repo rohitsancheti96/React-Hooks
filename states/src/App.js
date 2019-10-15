73fefa04e7ef66ab830b20b605e1c34a27b58d21
Rohit Sancheti <rohitsancheti96@gmail.com>
2019-10-15 15:25:18 +0530

    Initial

---
+++ states/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import HookCounter from "./components/HookCounter";
+import HookCounterTwo from "./components/HookCounterTwo";
+
+function App() {
+  return (
+    <div className="App">
+      <HookCounterTwo />
+    </div>
+  );
+}
+
+export default App;
---
+++ states/src/components/HookCounterTwo.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+function HookCounterTwo() {
+  const initialCount = 0;
+  const [count, setCount] = useState(initialCount);
+
+  const incrementFive = () => {
+    for (let i = 0; i < 5; i++) {
+      setCount(prevCount => prevCount + 1);
+    }
+  };
+
+  return (
+    <div>
+      Count: {count}
+      <button onClick={() => setCount(initialCount)}>Reset</button>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
+      <button onClick={incrementFive}>+5</button>
+    </div>
+  );
+}
+
+export default HookCounterTwo;
